a46efa872b299a07d6c70a84e32c730942f7774e
IsaquePinheiro <isaquesp@gmail.com>
2023-09-03 10:34:53 -0300

    documentation

--- Source/eclbr.sysdictionary.pas
+++ Source/eclbr.sysdictionary.pas
@@ -41,36 +41,214 @@ type
     function _ComparePairs(const Left, Right: TPair<K, V>): Integer;
     procedure SortByKey(const ASelector: TFunc<K, V, TValue>);
   public
+    /// <summary>
+    ///   Adds the key-value pairs from another dictionary to this dictionary.
+    /// </summary>
+    /// <param name="ASource">The source dictionary containing key-value pairs to add.</param>
     procedure AddDictionary(const ASource: TDictionary<K,V>);
+
+    /// <summary>
+    ///   Iterates through each key-value pair in the dictionary and performs an action on them.
+    /// </summary>
+    /// <param name="AAction">The action to be performed on each key-value pair.</param>
     procedure ForEach(const AAction: TProc<K, V>);
+
+    /// <summary>
+    ///   Iterates through each key-value pair in the dictionary along with its index and performs an action on them.
+    /// </summary>
+    /// <param name="AAction">The action to be performed on each indexed key-value pair.</param>
     procedure ForEachIndexed(const AAction: TProc<Integer, K, V>);
+
+    /// <summary>
+    ///   Rotates the dictionary by a specified number of positions.
+    /// </summary>
+    /// <param name="ACount">The number of positions to rotate the dictionary.</param>
     procedure Rotate(const ACount: Integer);
+
+    /// <summary>
+    ///   Removes duplicate values from the dictionary, keeping only the first occurrence of each value.
+    /// </summary>
     procedure Unique;
+
+    /// <summary>
+    ///   Returns the sorted keys of the dictionary.
+    /// </summary>
     function SortedKeys: TArray<K>;
+
+    /// <summary>
+    ///   Shuffles the keys of the dictionary.
+    /// </summary>
     function ShuffleKeys: TArray<K>;
+
+    /// <summary>
+    ///   Maps the values of the dictionary to a new dictionary of results.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the mapped results.</typeparam>
+    /// <param name="AMappingFunc">The mapping function to be applied to each value.</param>
     function Map<TResult>(const AMappingFunc: TFunc<V, TResult>): TDictionaryHelper<K, TResult>;
+
+    /// <summary>
+    ///   Filters the values of the dictionary based on a specified predicate.
+    /// </summary>
+    /// <param name="APredicate">The predicate used to filter the values.</param>
     function Filter(const APredicate: TFunc<V, Boolean>): TDictionaryHelper<K, V>;
+
+    /// <summary>
+    ///   Reduces the values of the dictionary to a single value using an accumulator function.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the reduction result.</typeparam>
+    /// <param name="AAccumulator">The accumulator function used to reduce the values.</param>
     function Reduce(const AAccumulator: TFunc<V, V, V>): V;
+
+    /// <summary>
+    ///   Groups the values of the dictionary by a key selector and returns a new dictionary with grouped value lists.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the grouping key.</typeparam>
+    /// <param name="AKeySelector">The key selector function.</param>
     function GroupBy<TKey>(const AKeySelector: TFunc<V, TKey>): TDictionary<TKey, TList<V>>;
+
+    /// <summary>
+    ///   Joins the values of the dictionary into a single string, separated by the specified separator.
+    /// </summary>
+    /// <param name="ASeparator">The separator used to join the values.</param>
     function Join(const ASeparator: string): string;
+
+    /// <summary>
+    ///   Partitions the dictionary into two dictionaries based on a given predicate.
+    /// </summary>
+    /// <param name="APredicate">The predicate used to partition the dictionary.</param>
+    /// <returns>
+    ///   A pair of dictionaries where the first dictionary contains key-value pairs that satisfy the predicate,
+    ///   and the second dictionary contains key-value pairs that do not satisfy the predicate.
+    /// </returns>
     function Partition(const APredicate: TFunc<V, Boolean>): TPair<TDictionaryHelper<K, V>, TDictionaryHelper<K, V>>;
+
+    /// <summary>
+    ///   Takes a specified number of key-value pairs from the beginning of the dictionary.
+    /// </summary>
+    /// <param name="ACount">The number of key-value pairs to take.</param>
+    /// <returns>A new dictionary with the specified number of key-value pairs from the start.</returns>
     function Take(const ACount: Integer): TDictionaryHelper<K, V>;
+
+    /// <summary>
+    ///   Skips a specified number of key-value pairs from the beginning of the dictionary and returns the remaining pairs.
+    /// </summary>
+    /// <param name="ACount">The number of key-value pairs to skip.</param>
+    /// <returns>A new dictionary with key-value pairs after skipping the specified number of pairs.</returns>
     function Skip(const ACount: Integer): TDictionaryHelper<K, V>;
+
+    /// <summary>
+    ///   Creates a new dictionary containing key-value pairs from the specified start index to the end index.
+    /// </summary>
+    /// <param name="AStartIndex">The starting index for slicing.</param>
+    /// <param name="AEndIndex">The ending index for slicing.</param>
+    /// <returns>A new dictionary with the sliced key-value pairs.</returns>
     function Slice(AStartIndex, AEndIndex: Integer): TDictionaryHelper<K, V>;
+
+    /// <summary>
+    ///   Combines two dictionaries with a specified function to create a new dictionary.
+    /// </summary>
+    /// <typeparam name="T1">The value type of the first dictionary.</typeparam>
+    /// <typeparam name="T2">The value type of the second dictionary.</typeparam>
+    /// <typeparam name="TResult">The value type of the result dictionary.</typeparam>
+    /// <param name="AList1">The first dictionary to combine.</param>
+    /// <param name="AList2">The second dictionary to combine.</param>
+    /// <param name="AFunc">The function to apply to each pair of values.</param>
+    /// <returns>A new dictionary with values resulting from applying the function to corresponding pairs.</returns>
     function Zip<T1, T2, TResult>(const AList1: TDictionaryHelper<K, T1>; const AList2: TDictionaryHelper<K, T2>;
       const AFunc: TFunc<T1, T2, TResult>): TDictionaryHelper<K, TResult>;
+
+    /// <summary>
+    ///   Maps each value in the dictionary to an array of results and flattens the results into a single dictionary.
+    /// </summary>
+    /// <typeparam name="TResult">The value type of the result dictionary.</typeparam>
+    /// <param name="AFunc">The function to map each value to an array of results.</param>
+    /// <returns>A new dictionary containing flattened results.</returns>
     function FlatMap<TResult>(const AFunc: TFunc<TValue, TArray<TResult>>): TDictionaryHelper<K, TResult>;
+
+    /// <summary>
+    ///   Returns a new dictionary containing key-value pairs that are common between two dictionaries.
+    /// </summary>
+    /// <param name="AOtherDict">The other dictionary to intersect with.</param>
+    /// <returns>A new dictionary containing common key-value pairs.</returns>
     function Intersect(const AOtherDict: TDictionaryHelper<K, V>): TDictionaryHelper<K, V>;
+
+    /// <summary>
+    ///   Returns a new dictionary containing key-value pairs that exist in the current dictionary but not in another dictionary.
+    /// </summary>
+    /// <param name="AOtherDict">The other dictionary to compare.</param>
+    /// <returns>A new dictionary containing key-value pairs not present in the other dictionary.</returns>
     function &Except(const AOtherDict: TDictionaryHelper<K, V>): TDictionaryHelper<K, V>;
+
+    /// <summary>
+    ///   Returns the maximum key in the dictionary.
+    /// </summary>
+    /// <returns>The maximum key in the dictionary.</returns>
     function Max: K;
+
+    /// <summary>
+    ///   Returns the minimum key in the dictionary.
+    /// </summary>
+    /// <returns>The minimum key in the dictionary.</returns>
     function Min: K;
+
+    /// <summary>
+    ///   Returns a new dictionary with distinct keys based on the selected key selector function.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key used for distinct selection.</typeparam>
+    /// <param name="AKeySelector">The function used to select keys for distinct values.</param>
+    /// <returns>A new dictionary with distinct keys.</returns>
     function DistinctBy<TKey>(const AKeySelector: TFunc<K, TKey>): TDictionary<TKey, V>;
+
+    /// <summary>
+    ///   Returns a new dictionary containing key-value pairs that satisfy the given predicate.
+    /// </summary>
+    /// <param name="APredicate">The predicate used to filter key-value pairs.</param>
+    /// <returns>A new dictionary containing filtered key-value pairs.</returns>
     function FindAll(const APredicate: TFunc<V, Boolean>): TDictionary<K, V>;
+
+    /// <summary>
+    ///   Returns a new dictionary containing key-value pairs from the beginning of the dictionary
+    ///   while the specified predicate is true.
+    /// </summary>
+    /// <param name="APredicate">The predicate used to take key-value pairs.</param>
+    /// <returns>A new dictionary containing key-value pairs that match the predicate.</returns>
     function TakeWhile(const APredicate: TFunc<K, Boolean>): TDictionary<K, V>;
+
+    /// <summary>
+    ///   Skips key-value pairs from the beginning of the dictionary while the specified predicate is true
+    ///   and returns the remaining key-value pairs.
+    /// </summary>
+    /// <param name="APredicate">The predicate used to skip key-value pairs.</param>
+    /// <returns>A new dictionary containing key-value pairs after skipping while the predicate is true.</returns>
     function SkipWhile(const APredicate: TFunc<K, Boolean>): TDictionary<K, V>;
+
+    /// <summary>
+    ///   Partitions the dictionary into two groups based on the given predicate.
+    ///   Keys that satisfy the predicate are placed in one group, and keys that do not satisfy it are placed in another group.
+    /// </summary>
+    /// <param name="APredicate">The predicate used for partitioning.</param>
+    /// <returns>
+    ///   A new dictionary with two entries: one entry for keys that satisfy the predicate (true) and another for keys that do not (false).
+    /// </returns>
     function PartitionBy(const APredicate: TFunc<V, Boolean>): TDictionary<Boolean, TList<V>>;
+
+    /// <summary>
+    ///   Returns the last key in the dictionary.
+    /// </summary>
+    /// <returns>The last key in the dictionary.</returns>
     function LastKey: K;
+
+    /// <summary>
+    ///   Returns the last value in the dictionary.
+    /// </summary>
+    /// <returns>The last value in the dictionary.</returns>
     function LastValue: V;
+
+    /// <summary>
+    ///   Returns a string representation of the dictionary.
+    /// </summary>
+    /// <returns>A string representation of the key-value pairs in the dictionary.</returns>
     function ToString: string; override;
   end;
 
